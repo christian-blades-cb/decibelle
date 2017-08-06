66c4db8b258ce325a14ca1e3a82df632acb4e0b1
Christian Blades <cblades@pindrop.com>
2017-08-05 22:04:42 -0400

    now I'm getting -8192 for every read???

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ fn main() {
         let err = alsa_sys::snd_pcm_open(&mut handle,
                                          device.as_ptr(),
                                          alsa_sys::SND_PCM_STREAM_CAPTURE,
-                                         alsa_sys::SND_PCM_NONBLOCK);
+                                         // alsa_sys::SND_PCM_NONBLOCK);
+                                         0);
         if err < 0 {
             panic!("device not available");
         }
@@ -39,10 +40,21 @@ fn main() {
             panic!("unable to setup pcm capture");
         }
 
+        alsa_sys::snd_pcm_prepare(handle);
         debug!("setup complete");
 
-        let buffer: &[i16] = &[0; 8 * 1024];
-        let bufptr = buffer.as_ptr() as *mut libc::c_void;
+        debug!("waiting for ready");
+        match alsa_sys::snd_pcm_wait(handle, 30000) {
+            0 => {
+                alsa_sys::snd_pcm_close(handle);
+                panic!("timeout waiting for pcm ready")
+            }
+            1 => debug!("ready!"),
+            _ => debug!("wut?"),
+        };
+
+        let mut buffer: &mut [i16] = &mut [0; 8 * 1024];
+        let bufptr: *mut libc::c_void = buffer.as_mut_ptr() as *mut libc::c_void;
         let bufsize = buffer.len() as alsa_sys::snd_pcm_uframes_t;
 
         loop {
@@ -50,13 +62,24 @@ fn main() {
                 println!("signal caught!");
                 break;
             }
-
+            // debug!("filling buffer");
             let frames = alsa_sys::snd_pcm_readi(handle, bufptr, bufsize);
-            debug!("frames {}", frames);
-            let sums: i16 = (0..frames).map(|x| buffer[x as usize] * buffer[x as usize]).sum();
-            let rms = (sums as f64).sqrt();
-            format!("{}\n", rms);
-            info!("loop'd");
+            // debug!("frames {}", frames);
+            if frames == -11 {
+                debug!("EAGAIN");
+                continue;
+            }
+            if frames > 0 {
+                debug!("errno {}", -frames);
+                continue;
+            }
+
+            let sums: f64 =
+                (0..frames).map(|x| buffer[x as usize] as f64 * buffer[x as usize] as f64).sum();
+            let rms = sums.sqrt();
+            println!("rms {}", rms);
+
+            // info!("loop'd");
         }
 
         alsa_sys::snd_pcm_close(handle);
