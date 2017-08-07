4bc95892c67c2a311785bc40ebe8d0aefce2a950
Christian Blades <cblades@pindrop.com>
2017-08-07 17:53:02 -0400

    async, much nicer to the CPU

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate portaudio;
 extern crate log;
 extern crate env_logger;
 
-// use std::collections::VecDeque;
+use std::{thread,time};
 
 const SAMPLE_RATE: f64 = 44_100.0;
 const CHANNELS: i32 = 2;
@@ -14,7 +14,7 @@ const INTERLEAVED: bool = true;
 // 0.0790569415 - https://github.com/yorban/SmartSoundMeasurement/blob/master/app/src/main/java/com/csau/smartsound/splmeasurement/SmartSoundMeasurement.java
 // 0.45255; // from https://github.com/johnliu55tw/ALSASoundMeter/blob/master/sound_meter.c
 const K: f64 = 0.0790569415;
-const BASE: f64 = 110.0;
+const BASE: f64 = 110.0; // very scientific, not calibrated at all
 
 fn main() {
     env_logger::init().unwrap();
@@ -35,54 +35,29 @@ fn main() {
     let input_params =
         portaudio::StreamParameters::<f32>::new(input, CHANNELS, INTERLEAVED, latency);
     let stream_settings = portaudio::stream::InputSettings::new(input_params, SAMPLE_RATE, FRAMES);
-    let mut stream = pa.open_blocking_stream(stream_settings).unwrap();
 
-    // let mut buffer: VecDeque<f32> = VecDeque::with_capacity(FRAMES as usize * CHANNELS as usize);
+    let mut stream = pa.open_non_blocking_stream(stream_settings, audio_callback).unwrap();
 
     debug!("starting stream");
     stream.start().unwrap();
 
-    // We'll use this function to wait for read/write availability.
-    fn wait_for_stream<F>(f: F, name: &str) -> u32
-        where F: Fn() -> Result<portaudio::StreamAvailable, portaudio::error::Error>
-    {
-        'waiting_for_stream: loop {
-            match f() {
-                Ok(available) => {
-                    match available {
-                        portaudio::StreamAvailable::Frames(frames) => return frames as u32,
-                        portaudio::StreamAvailable::InputOverflowed => {
-                            println!("Input stream has overflowed")
-                        }
-                        portaudio::StreamAvailable::OutputUnderflowed => {
-                            println!("Output stream has underflowed")
-                        }
-                    }
-                }
-                Err(err) => {
-                    panic!("An error occurred while waiting for the {} stream: {}",
-                           name,
-                           err)
-                }
-            }
-        }
-    };
-
-    'stream: loop {
+    loop {
         if sb.caught() {
-            println!("caught signal!");
+            info!("signal caught!");
             break;
         }
-
-        let frames = wait_for_stream(|| stream.read_available(), "Read");
-        if frames > 0 {
-            let samples: &[f32] = stream.read(frames).unwrap();
-            let sum_squares: f64 = samples.into_iter().map(|&x| x as f64 * x as f64).sum();
-            let rms = (sum_squares / samples.len() as f64).sqrt();
-            let db = BASE + (20.0 * (K * rms).log10());
-            println!("rms: {} db: {}", rms, db);
-        }
+        thread::sleep(time::Duration::from_millis(100));
     }
     
     stream.close().unwrap();
+}
+
+fn audio_callback(args: portaudio::stream::InputCallbackArgs<f32>) -> portaudio::stream::CallbackResult {
+    if args.frames > 0 {
+        debug!("frames: {}", args.frames);
+        let sum_squares: f64 = args.buffer.into_iter().map(|&x| x as f64 * x as f64).sum();
+        let rms = (sum_squares / args.buffer.len() as f64).sqrt();
+        println!("rms: {}", rms);
+    }
+    return portaudio::Continue;
 }
